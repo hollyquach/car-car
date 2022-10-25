741b30cd9042e9470dd895b76e2b1c8ceead8d2e
Holly Quach <hollyquach@gmail.com>
2022-10-25 11:35:12 -0700

    Added URL patterns, model encoders & placeholders for views for service API microservice

--- service/api/service_rest/urls.py
+++ service/api/service_rest/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
+from .views import api_list_techs, api_list_appts, api_view_appts
 
-urlpatterns = []
+urlpatterns = [
+    path("tech/", api_list_techs, name="api_list_techs"),
+    path("services/", api_list_appts, name="api_list_appts"),
+    path("services/<str:vin>", api_view_appts, name="api_view_appts"),    
+]
--- service/api/service_rest/views.py
+++ service/api/service_rest/views.py
@@ -1,3 +1,58 @@
-from django.shortcuts import render
+from django.http import JsonResponse
+from django.views.decorators.http import require_http_methods
+import json
 
-# Create your views here.
+from .models import AutomobileVO, Technician, ServiceAppt
+from common.json import ModelEncoder
+
+
+# > Encoders
+class AutomobileVOEncoder(ModelEncoder):
+    model = AutomobileVO
+    properties = ["import_href", "id", "vin"]
+
+class TechnicianEncoder(ModelEncoder):
+    model = Technician
+    properties = ["href", "id", "name", "employee_number"]
+
+#?? same encoder for detail and list since the table display includes everything for both
+class ServiceApptEncoder(ModelEncoder):
+    model = ServiceAppt
+    properties = [
+        "href",
+        "id",
+        "vin",
+        "owner",
+        "date_time",
+        "tech",
+        "status",
+        "reason",
+        "vip",
+    ]
+
+# > Views
+
+# create new techs
+@require_http_methods(["GET", "POST"])
+def api_list_techs(request):
+    """
+    # [] update docstring description
+    """
+    pass
+
+# list all service appts & create new appts
+@require_http_methods(["GET", "POST"])
+def api_list_appts(request, automobile_vo_id = None):
+    """
+    # [] update docstring description
+    """
+    pass
+
+
+# list service appts by VIN & update appt status (cancelled/finished)
+@require_http_methods(["GET", "PUT"])
+def api_view_appts(request, automobile_vo_vin = None):
+    """
+    # [] update docstring description
+    """
+    pass

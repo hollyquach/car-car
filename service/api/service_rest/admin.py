84b493af5761944909218e78b583b91e1a9b3aa4
Holly Quach <hollyquach@gmail.com>
2022-10-25 17:24:56 +0000

    Autoservice - Models

--- service/api/service_rest/admin.py
+++ service/api/service_rest/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import  AutomobileVO, Technician, ServiceAppt
 
-# Register your models here.
+admin.site.register(AutomobileVO)
+admin.site.register(Technician)
+admin.site.register(ServiceAppt)
--- service/api/service_rest/models.py
+++ service/api/service_rest/models.py
@@ -1,3 +1,34 @@
+from asyncio.base_futures import _FINISHED
 from django.db import models
 
-# Create your models here.
+class AutomobileVO(models.Model):
+    import_href = models.CharField(max_length = 200, unique = True)
+    auto_vin = models.CharField(max_length = 20)
+
+
+class Technician(models.Model):
+    name = models.CharField(max_length = 100)
+    employee_number = models.PositiveIntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class ServiceAppt(models.Model):
+    vin = models.CharField(max_length = 20)
+    owner = models.CharField(max_length = 100)
+    date_time = models.DateTimeField(auto_now=False, auto_now_add=False)
+    tech = models.ManyToManyField(Technician, related_name= "ServiceAppt")
+    STATUS_CHOICES = [
+        ('PENDING', 'Pending'),
+        ('FINISHED', 'Finished'),
+        ('CANCELLED', 'Cancelled')
+    ]
+    status = models.CharField(max_length = 10, choices = STATUS_CHOICES, default = 'PENDING')
+    reason = models.TextField()
+    vip = models.BooleanField(default = False)
+
+    def __str__(self):
+        return f'({self.vin} | {date_time} | status)'
+
+

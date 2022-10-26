de3cb2f4ff9b4da4f38f7cfabbe67598d99b36c3
Holly Quach <hollyquach@gmail.com>
2022-10-26 15:50:38 -0700

    added files for main index of each service - includes nav with specific links for each.

---
+++ ghi/app/src/inventory/inventoryMain.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
+export default function Inventory() {
+    return (
+        <>
+            <h1>Inventory</h1>
+            <nav className="navbar">
+                <div class="container-fluid">
+                    <ul className="nav nav-tabs justify-content-end flex-column flex-sm-row">
+                        <li className="nav-item p-1">
+                            <NavLink className="nav-link" aria-current="page" to="/inventory/manufacturers">Manufacturers</NavLink>
+                        </li>
+                        <li className="nav-item p-1">
+                            <NavLink className="nav-link" aria-current="page" to="/inventory/models">Vehicle Models</NavLink>
+                        </li>
+                        <li className="nav-item p-1">
+                            <NavLink className="nav-link" aria-current="page" to="/inventory/automobiles">Automobiles</NavLink>
+                        </li>
+                        <li className="nav-item dropdown p-1">
+                            <NavLink className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                New
+                            </NavLink>
+                            <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <li><NavLink className="dropdown-item" to="/inventory/manufacturers/new">Manufacturer</NavLink></li>
+                                <li><NavLink className="dropdown-item" to="/inventory/models/new">Model</NavLink></li>
+                                <li><NavLink className="dropdown-item" to="/inventory/automobiles/new">Automobile</NavLink></li>
+                            </ul>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+            <Outlet />
+        </>
+    )
+}
+
---
+++ ghi/app/src/service/servicesMain.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+
+//!! Service List
+/*
+Common Fields
+    - VIN
+    - Customer Name
+    - Date
+    - Time
+    - Technician
+    - Reason
+    - Appointments List
+    - Cancel >> button (hook)
+    - Finished >> button (hook)
+Service History (filtered by VIN)
+    - Search >> endpoint >> /api/services?vin=<:vin>
+*/
+
+export default function Service() {
+    return (
+        <>
+            <h1>Service</h1>
+                <nav className="navbar">
+                    <div class="container-fluid">
+                        <ul className="nav nav-tabs justify-content-end flex-column flex-sm-row">
+                            <li className="nav-item p-2">
+                                <NavLink className="nav-link" aria-current="page" to="/service">Service Appointments</NavLink>
+                            </li>
+                            <li className="nav-item dropdown p-2">
+                                <NavLink className="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    New
+                                </NavLink>
+                                <ul className="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><NavLink className="dropdown-item" to="/service/appointment">Appointment</NavLink></li>
+                                    <li><NavLink className="dropdown-item" to="/service/technician">Technician</NavLink></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </nav>
+            <Outlet />
+        </>
+    )
+
+}

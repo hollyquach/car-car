7214828c4cfc1fc74e7f749e5c84be691d3f02b3
Holly Quach <hollyquach@gmail.com>
2022-10-28 11:48:41 -0700

    created manufacturer form

--- ghi/app/src/inventory/forms/manufacturerForm.js
+++ ghi/app/src/inventory/forms/manufacturerForm.js
@@ -1,35 +1,59 @@
 import React from 'react';
 
 export default class MfgForm extends React.Component {
-    // > non-functional -> drafted outlined only
-        // constructor -> define props
+    constructor(props) {
+        super(props)
+        this.state = {
+            name: '',
+        };
+    }
 
-        // handle Submit
 
-        // handle property changes (with submit)
+    handleSubmit = async (e) => {
+        e.preventDefault();
+        const data = { ...this.state };
 
-        // mount component -> ref data models
+        const mfgURL = 'http://localhost:8100/api/manufacturers/';
+        const fetchConfig = {
+            method: "POST",
+            body: JSON.stringify(data),
+            headers: {
+                'Content-Type': 'application/json',
+            },
+        };
 
-        // render return
-        render() {
-            
-            return (
-                <div className="my-5 container">
+        const response = await fetch(mfgURL, fetchConfig);
+        if (response.ok) {
+            await response.json();
+            this.setState({name: ''});
+        } else {
+            console.error("MANUFACTURER FORM SUBMIT ERROR !!")
+        }
+    }
+
+    handleNameChange = (event) => {
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+
+    render() {
+        return (
             <div className="row">
-            <div className="col">
-                <div className="card shadow">
-                <div className="card-body">
-                    <form className="form" id="create-manufacturer-form">
-                    <h3 className="card-title">NEW CAR MANUFACTURER FORM PLACEHOLDER</h3>
-                    <p className="mb-3">
-                        BLAH BLAH BLAH
-                    </p>
-                    </form>
-                </div>
+                <div className="offset-3 col-6">
+                    <div className="shadow p-4 mt-4">
+                        <h3 className="card-title">Create new Manufacturer</h3>
+                        <form onSubmit={this.handleSubmit} className="form" id="create-manufacturer-form">
+                            <div className="form-floating mb-3">
+                                <input placeholder="name" name="name" required type="name" className="form-control" value={this.state.name} onChange={this.handleNameChange}/>
+                                <label htmlFor="name">Name</label>
+                            </div>
+                            <button className="btn btn-primary">Create</button>
+                        </form>
+                    </div>
                 </div>
             </div>
-            </div>
-        </div>
         );
     }
 }
